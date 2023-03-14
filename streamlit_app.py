8460be7a9a9832bdc53660a8e85655fcc8c29e83
mpolanco-ifyl <123343881+mpolanco-ifyl@users.noreply.github.com>
2023-03-14 14:20:44 -0600

    Create

---
+++ streamlit_app.py
@@ -0,0 +1,50 @@
+import os
+import openai
+import streamlit as st
+import pdfplumber
+from io import BytesIO
+from openai_secret_manager import secrets_manager
+
+# Configura OpenAI API
+openai.api_key = secrets_manager.get_secret("openai")["api_key"]
+
+# Función para extraer texto de un archivo PDF
+def extract_text_from_pdf(file):
+    with pdfplumber.open(file) as pdf:
+        pages = [page.extract_text() for page in pdf.pages]
+        return "\n".join(pages)
+
+# Función para generar respuesta utilizando GPT-3.5-turbo
+def generate_answer(prompt, model="gpt-3.5-turbo"):
+    completions = openai.Completion.create(
+        engine=model,
+        prompt=prompt,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = completions.choices[0].text.strip()
+    return message
+
+# Interfaz de Streamlit
+st.title("Lector de PDF y Asistente de Preguntas con GPT-3.5-turbo")
+
+uploaded_file = st.file_uploader("Sube un archivo PDF", type=["pdf"])
+
+if uploaded_file:
+    with st.spinner("Extrayendo texto del archivo PDF..."):
+        pdf_text = extract_text_from_pdf(uploaded_file)
+
+    st.write("Texto extraído:")
+    st.write(pdf_text)
+
+    question = st.text_input("Escribe tu pregunta:")
+
+    if question:
+        with st.spinner("Generando respuesta..."):
+            prompt = f"El siguiente texto fue extraído de un PDF:\n\n{pdf_text}\n\nPregunta: {question}\nRespuesta:"
+            answer = generate_answer(prompt)
+            st.write("Respuesta:")
+            st.write(answer)
